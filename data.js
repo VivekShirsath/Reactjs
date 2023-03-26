62623f34f4f6d7581acf40cec13b91979ee182dc
Vivek Shirsath <vivekshirsath1310@gmail.com>
2023-03-26 11:49:38 +0530

    Using Props

---
+++ data.js
@@ -0,0 +1,646 @@
+
+export const restaurants = [{
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "512174",
+      "name": "Bhai Bhai Chicken Corner",
+      "uuid": "542eca1e-44d6-4724-baaa-ad4fb7d7201e",
+      "city": "21",
+      "area": "Pandesara",
+      "totalRatingsString": "Too Few Ratings",
+      "cloudinaryImageId": "jg53c9sxlrjokn3rocgz",
+      "cuisines": [
+        "North Indian",
+        "Indian"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 20000,
+      "costForTwoString": "₹200 FOR TWO",
+      "deliveryTime": 38,
+      "minDeliveryTime": 38,
+      "maxDeliveryTime": 38,
+      "slaString": "38 MINS",
+      "lastMileTravel": 10.5,
+      "slugs": {
+        "restaurant": "bhai-bhai-chicken-corner-dindoli-dindoli",
+        "city": "surat"
+      },
+      "cityState": "21",
+      "address": "45 AMBIKA NAGAR PANDESARA SURAT 39421",
+      "locality": "Bhedwad",
+      "parentId": 306383,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 6200,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 6200,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "6200",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 1,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "10.5 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "512174",
+        "deliveryTime": 38,
+        "minDeliveryTime": 38,
+        "maxDeliveryTime": 38,
+        "lastMileTravel": 10.5,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "IT_IS_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "--",
+      "totalRatings": 0,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "87882",
+      "name": "Tulsi Restaurant",
+      "uuid": "ff612e9a-7797-431b-8ad7-f9861cb4998f",
+      "city": "21",
+      "area": "Udhna Gam",
+      "totalRatingsString": "1000+ ratings",
+      "cloudinaryImageId": "wqeyjwo0juu0eyllxwqt",
+      "cuisines": [
+        "Chinese",
+        "North Indian",
+        "Biryani"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 18000,
+      "costForTwoString": "₹180 FOR TWO",
+      "deliveryTime": 32,
+      "minDeliveryTime": 32,
+      "maxDeliveryTime": 32,
+      "slaString": "32 MINS",
+      "lastMileTravel": 11.399999618530273,
+      "slugs": {
+        "restaurant": "tulsi-restaurant-gurudwara-parvat-patiya-parvat-patiya",
+        "city": "surat"
+      },
+      "cityState": "21",
+      "address": "1st Floor, Udhna Bus Depot, Nr. Gurudwara, Surat Navsari Main, Surat 394210.",
+      "locality": "Surat Navsari",
+      "parentId": 218330,
+      "unserviceable": false,
+      "veg": true,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "20% off",
+        "shortDescriptionList": [
+          {
+            "meta": "20% off above Rs",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          },
+          {
+            "meta": "Free Veg  Raita ",
+            "discountType": "Freebie",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "20% off on all orders",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          },
+          {
+            "meta": "Free Veg  Raita  on orders above Rs 499",
+            "discountType": "Freebie",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "20% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "20% off on all orders",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          },
+          {
+            "meta": "Free Veg  Raita  on orders above Rs 499",
+            "discountType": "Freebie",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 6200,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 6200,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "6200",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 1,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "11.3 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "87882",
+        "deliveryTime": 32,
+        "minDeliveryTime": 32,
+        "maxDeliveryTime": 32,
+        "lastMileTravel": 11.399999618530273,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "IT_IS_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "3.9",
+      "totalRatings": 1000,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "514729",
+      "name": "La Pino'z Pizza",
+      "uuid": "84416cae-29af-4b85-ae8b-5c28358a65c6",
+      "city": "21",
+      "area": "Udhna Gam",
+      "totalRatingsString": "1000+ ratings",
+      "cloudinaryImageId": "i0wdxy1pfb7zhyuivtow",
+      "cuisines": [
+        "Pizzas",
+        "Beverages",
+        "Pastas"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 30000,
+      "costForTwoString": "₹300 FOR TWO",
+      "deliveryTime": 34,
+      "minDeliveryTime": 34,
+      "maxDeliveryTime": 34,
+      "slaString": "34 MINS",
+      "lastMileTravel": 11.800000190734863,
+      "slugs": {
+        "restaurant": "la-pino’z-pizza-udhna-udhna",
+        "city": "surat"
+      },
+      "cityState": "21",
+      "address": "276/277/2 PLOT NO-52, BUILDING-A, SHOP NO-G-14 & G-16, GR FLOOR, SHASHWAT RESIDENCY, UDHNA, Surat City,394210",
+      "locality": "Shanti Nagar",
+      "parentId": 4961,
+      "unserviceable": false,
+      "veg": true,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 6200,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 6200,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "6200",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 1,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "11.8 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "514729",
+        "deliveryTime": 34,
+        "minDeliveryTime": 34,
+        "maxDeliveryTime": 34,
+        "lastMileTravel": 11.800000190734863,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "IT_IS_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "3.9",
+      "totalRatings": 1000,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "221197",
+      "name": "Nv Fastfood",
+      "uuid": "41074d8e-bdae-49d2-9fc5-f31055313a0a",
+      "city": "21",
+      "area": "Udhna Gam",
+      "totalRatingsString": "1000+ ratings",
+      "cloudinaryImageId": "yu78pjchxpwfmotzuw8u",
+      "cuisines": [
+        "Chinese"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 15000,
+      "costForTwoString": "₹150 FOR TWO",
+      "deliveryTime": 35,
+      "minDeliveryTime": 35,
+      "maxDeliveryTime": 35,
+      "slaString": "35 MINS",
+      "lastMileTravel": 11.800000190734863,
+      "slugs": {
+        "restaurant": "nv-fastfood-athwa-athwa",
+        "city": "surat"
+      },
+      "cityState": "21",
+      "address": "Shop G-2, Saswat Residency, Santi Nagar, Udhna, Surat",
+      "locality": "Santi Nagar",
+      "parentId": 150693,
+      "unserviceable": false,
+      "veg": true,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "40% off",
+        "shortDescriptionList": [
+          {
+            "meta": "40% off | Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "40% off up to ₹80 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "40% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "40% off up to ₹80 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 6200,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 6200,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "6200",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 1,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "11.8 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "221197",
+        "deliveryTime": 35,
+        "minDeliveryTime": 35,
+        "maxDeliveryTime": 35,
+        "lastMileTravel": 11.800000190734863,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "IT_IS_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "3.9",
+      "totalRatings": 1000,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "642048",
+      "name": "Dos and Donut",
+      "uuid": "8b12954f-a98c-4e9a-807b-abed48e5c7fd",
+      "city": "21",
+      "area": "Udhna Gam",
+      "totalRatingsString": "Too Few Ratings",
+      "cloudinaryImageId": "ait1aprgkdkjv0akqs4s",
+      "cuisines": [
+        "Desserts"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 20000,
+      "costForTwoString": "₹200 FOR TWO",
+      "deliveryTime": 37,
+      "minDeliveryTime": 37,
+      "maxDeliveryTime": 37,
+      "slaString": "37 MINS",
+      "lastMileTravel": 11.5,
+      "slugs": {
+        "restaurant": "dos-and-donut-athwa-athwa",
+        "city": "surat"
+      },
+      "cityState": "21",
+      "address": "FLAT NO 306, PADMANAGAR ESTATE PATEL COLONY, BEHIND DENA BANK UDHNA, SURAT, Surat, Gujarat",
+      "locality": "Patel Colony",
+      "parentId": 387086,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 6200,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 6200,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "6200",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 1,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "11.5 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "642048",
+        "deliveryTime": 37,
+        "minDeliveryTime": 37,
+        "maxDeliveryTime": 37,
+        "lastMileTravel": 11.5,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "IT_IS_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "--",
+      "totalRatings": 0,
+      "new": false
+    },
+    "subtype": "basic"
+  }]
